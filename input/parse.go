ff3aaad5faf5ad24130f8e030d8e41c71b2a64d4
Garrett Powell <garrett@gpowell.net>
2018-02-15 18:47:02 -0500

    Remove magic constants

--- input/parse.go
+++ input/parse.go
@@ -12,14 +12,21 @@ import (
 
 const sizePattern string = `^(?i)([0-9]+)\s*([KMG])(B|iB)?$`
 const durationPattern string = `(?i)([0-9]+)\s*([hdmy])`
+
 const (
-	Hour time.Duration = time.Hour
-	Day time.Duration = Hour * 24
-	Month time.Duration = Day * 30
-	Year time.Duration = Month * 12
+	durationHour time.Duration = time.Hour
+	durationDay time.Duration = durationHour * 24
+	durationMonth time.Duration = durationDay * 30
+	durationYear time.Duration = durationMonth * 12
 )
 
-func FileSize(size string) (numBytes int64, err error) {
+// The keys in these maps must be lower-case.
+var sizePrefixes = map[string]int{"k": 1, "m": 2, "g": 3, "t": 4, "p": 5, "e": 6, "z": 7, "y": 8}
+var durationUnits = map[string]time.Duration{"h": durationHour, "d": durationDay, "m": durationMonth, "y": durationYear}
+
+// ReadFileSize parses a human-readable file size string and returns a number
+// of bytes. It recognizes both metric and binary units.
+func ReadFileSize(size string) (numBytes int64, err error) {
 	sizeRegex, err := regexp.Compile(sizePattern)
 	if err != nil {
 		log.Fatal(err)
@@ -34,8 +41,6 @@ func FileSize(size string) (numBytes int64, err error) {
 	unit = strings.ToLower(unit)
 
 	var base float64
-	var exponent float64
-
 	switch unit {
 	case "", "ib":
 		base = 1024
@@ -43,14 +48,7 @@ func FileSize(size string) (numBytes int64, err error) {
 		base = 1000
 	}
 
-	switch prefix {
-	case "k":
-		exponent = 1
-	case "m":
-		exponent = 2
-	case "g":
-		exponent = 3
-	}
+	exponent := float64(sizePrefixes[prefix])
 
 	coefficient, err := strconv.Atoi(num)
 	if err != nil {
@@ -60,7 +58,9 @@ func FileSize(size string) (numBytes int64, err error) {
 	return int64(coefficient * int(math.Pow(base, exponent))), nil
 }
 
-func Duration(duration string) (time.Duration, error) {
+// ReadDuration parses a human-readable duration string. It recognizes the
+// units "h", "d", "m", and "y".
+func ReadDuration(duration string) (time.Duration, error) {
 	durationRegex, err := regexp.Compile(durationPattern)
 	if err != nil {
 		log.Fatal(err)
@@ -76,16 +76,7 @@ func Duration(duration string) (time.Duration, error) {
 		}
 		unit = strings.ToLower(unit)
 
-		switch unit {
-		case "h":
-			output += time.Duration(num) * Hour
-		case "d":
-			output += time.Duration(num) * Day
-		case "m":
-			output += time.Duration(num) * Month
-		case "y":
-			output += time.Duration(num) * Year
-		}
+		output += time.Duration(num) + durationUnits[unit]
 	}
 
 	return output, nil
